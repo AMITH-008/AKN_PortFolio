8771e1b2b4ef9875b22ddc1b98356b77c03350be
amith <amithkouti@gmail.com>
2024-01-17 19:48:07 +0530

    Updated Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,9 +14,9 @@ const Computers = ( {isMobile} ) => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <spotLight position={[-20, 10, 50]} angle = {0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+      <spotLight position={[-20, 50, 10]} angle = {0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
       <primitive object={computer.scene}
-      scale={isMobile ?0.6:0.75} position={isMobile? [0,-3, -1]:[0, -4, -1.5]} 
+      scale={isMobile ?0.7:0.75} position={isMobile? [0,-3, -2.2]:[0, -3.25, -1.5]} 
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -41,6 +41,7 @@ const ComputersCanvas = () => {
   <Canvas 
   frameloop='demand'
   shadows
+  dpr={[1,2]}
   camera={{position:[20,3,5], fov:25}}
   gl={{preserveDrawingBuffer:true}}>
     <Suspense fallback={<CanvasLoader />}>
